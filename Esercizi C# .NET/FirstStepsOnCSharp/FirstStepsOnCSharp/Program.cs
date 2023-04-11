588c7dba6efa530aaaa37cced1dbc693137b81fc
UMBY2407 <umby2407@gmail.com>
2023-04-11 12:02:08 +0200

    Deleted a space in line  2/3

--- Esercizi C# .NET/FirstStepsOnCSharp/FirstStepsOnCSharp/Program.cs
+++ Esercizi C# .NET/FirstStepsOnCSharp/FirstStepsOnCSharp/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
 Console.WriteLine("Ciao a tutti! Come state?");
 var risposta = Console.ReadLine();
 Console.WriteLine("Grazie mille! Noto che stai " + risposta);
