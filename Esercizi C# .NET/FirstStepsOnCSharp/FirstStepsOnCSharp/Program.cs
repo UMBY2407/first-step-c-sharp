b07bff9dab7a66ae61f3871882d4d39c06f53325
UMBY2407 <umby2407@gmail.com>
2023-04-11 11:53:09 +0200

    Aggiungere i file di progetto.

---
+++ Esercizi C# .NET/FirstStepsOnCSharp/FirstStepsOnCSharp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Ciao a tutti! Come state?");
+var risposta = Console.ReadLine();
+Console.WriteLine("Grazie mille! Noto che stai " + risposta);
